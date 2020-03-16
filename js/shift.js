c26c7c163d43a0ee9dfca89de97b6bc0babcda58
bonochof <ryos427@yahoo.co.jp>
2020-03-17 03:03:24 +0900

    fix shift order

--- js/shift.js
+++ js/shift.js
@@ -1,14 +1,14 @@
 document.getElementById("__old").style.display = "none";
 
 function shift() {
-    const nuxt = document.getElementById("__nuxt");
-    const old = document.getElementById("__old");
+  const old = document.getElementById("__old");
+  const nuxt = document.getElementById("__nuxt");
 
-    if (nuxt.style.display == "block") {
-      nuxt.style.display = "none";
-      old.style.display = "block"
-    } else {
-      nuxt.style.display = "block";
-      old.style.display = "none";
-    }
+  if (old.style.display == "none") {
+    old.style.display = "block";
+    nuxt.style.display = "none";
+  } else {
+    old.style.display = "none";
+    nuxt.style.display = "block";
+  }
 }
