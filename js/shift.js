982d372fa8fc7908634c4290282d4477287ef082
bonochof <ryos427@yahoo.co.jp>
2020-03-24 01:58:49 +0900

    fix switch process

--- js/shift.js
+++ js/shift.js
@@ -1,11 +1,14 @@
-function shift() {
-  const old = document.getElementById("__old");
-  const nuxt = document.getElementById("__nuxt");
+function shift(oldIdx, nuxtIdx) {
+  const old = document.getElementById(oldIdx);
+  const nuxt = document.getElementById(nuxtIdx);
 
   if (old.style.display == "none") {
+    // nuxt -> old
+    location.replace(location.href);
     old.style.display = "block";
     nuxt.style.display = "none";
   } else {
+    // old -> nuxt
     old.style.display = "none";
     nuxt.style.display = "block";
   }
